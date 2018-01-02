496ca656e98cd047bdc650fd89ee58a1f1b8857a
bhartcivica <root@wtghart.civica.local>
2018-01-02 14:40:04 +0000

    Amended build scripts

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Build and deploy the Maven code to S3
-export AWS_ACCESS_KEY_ID=AKIAITAEU7UMVY5XRFRQ
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$ACCESS_KEY
 mvn -B compile
 mvn -B test
 mvn -B deploy -DskipTests
